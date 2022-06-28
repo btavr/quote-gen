4b67f59817bebe39268cb96e58b4ab1f33d4a5f3
btavr <brunotrodrigues98@gmail.com>
2022-06-29 00:43:05 +0100

    j

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState, } from "react"
 import './App.css';
 
 function App() {
+
+  const [quotes, setQuotes] = useState({})
+ 
+
+  const getQuote = () => {
+    fetch('https://type.fit/api/quotes')
+      .then((res) => res.json())
+      .then((data) => {
+        let random = Math.floor(Math.random() * data.length)
+        setQuotes(data[random])
+        
+      })
+  }
+
+  useEffect(() => {
+    getQuote()
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+      <div id='wrapper' >
+        <h1>Random Quote Machine</h1>
+           
+
+        <div id="quote-box">
+          <div id="elements">
+            <p id="text"><i id='quoteIcon' class="bi bi-quote"></i> {quotes.text}</p>
+            <p id="author">- {quotes.author}</p>
+            <button type="button" class="btn btn-outline-secondary" id="new-quote" onClick={getQuote}>new quote</button>
+            <a type="button" class="btn btn-outline-primary"
+              href={`https://twitter.com/intent/tweet?text="${quotes.text}" - ${quotes.author}`}
+              id="tweet-quote"><i class="bi bi-twitter"></i> tweet</a>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
